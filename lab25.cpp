816111bf7469437a47b3c7e6a255c06a8e6a6bbd
Smart309 <pattharapon_takham@cmu.ac.th>
2022-02-15 18:59:48 +0700

    "UP ALL R1"

--- lab25.cpp
+++ lab25.cpp
@@ -28,7 +28,78 @@ ComplexNumber ComplexNumber::operator-(const ComplexNumber &c){
 	return ComplexNumber(real-c.real,imag-c.imag);
 }
 
-//Write your code here
+ComplexNumber ComplexNumber::operator*(const ComplexNumber &c){
+	return ComplexNumber((real * c.real) - (imag * c.imag), (real * c.imag) + (imag * c.real));
+}
+
+ComplexNumber ComplexNumber::operator/(const ComplexNumber &c){
+	double R,I;
+	R = ((real*c.real)+(imag*c.imag))/(pow(c.real,2)+pow(c.imag,2));
+	I = ((real*(c.imag*-1))+(imag*c.real))/(pow(c.real,2)+pow(c.imag,2));
+	return ComplexNumber(R,I);
+}
+
+ComplexNumber operator+(double s ,const ComplexNumber &c){
+	return ComplexNumber(s+c.real,c.imag);
+}
+
+ComplexNumber operator-(double s ,const ComplexNumber &c){
+	return ComplexNumber(s-c.real,-c.imag);
+}
+
+ComplexNumber operator*(double s ,const ComplexNumber &c){
+	return ComplexNumber(s*c.real,s*c.imag);
+}
+
+ComplexNumber operator/(double s ,const ComplexNumber &c){
+	double R,I;
+	R = ((s*c.real))/(pow(c.real,2)+pow(c.imag,2));
+	I = ((s*(c.imag*-1)))/(pow(c.real,2)+pow(c.imag,2));
+	return ComplexNumber(R,I);
+}
+
+ostream & operator<<(ostream &os , const ComplexNumber & c){
+	if (c.real!= 0 && c.imag>0)
+	{
+		os<<c.real<<"+"<<c.imag<<"i";
+	}else if(c.real==0 || c.imag==0){
+		if(c.imag == 0)
+		{
+			os<<c.real;
+		}else if (c.real == 0){
+			os<<c.imag<<"i";
+		}
+	}else if(c.real!= 0 && c.imag<0){
+		os<<c.real<<c.imag<<"i";
+	}else{
+		os<<c.real;
+	}
+	return os;
+}
+
+bool ComplexNumber::operator==(const ComplexNumber &c){
+	if(real==c.real && imag == c.imag ){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+bool operator==(double s , const ComplexNumber &c){
+	if(s==c.real && s == c.imag ){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+double ComplexNumber::abs(){
+	return sqrt(pow(real,2)+pow(imag,2));
+}
+
+double ComplexNumber::angle(){
+	return atan2(imag,real) * 180 / M_PI;
+}
 
 int main(){
 	ComplexNumber a(1.5,2),b(3.2,-2.5),c(-1,1.2);	
